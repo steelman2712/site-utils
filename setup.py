8e40b700f183750be4d8db6c106b2cff0f277b4b
Dan Reynolds <dreynolds2712@gmail.com>
2021-03-12 17:25:23 +0100

    Changed files

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ print(name)
 setuptools.setup(
     name=name,
     version="0.0.1",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "steelforge_site_utils"},
+    packages=setuptools.find_packages(where="steelforge_site_utils"),
     python_requires=">=3.6",
 
 )
