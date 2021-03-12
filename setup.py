e1c9b9a402af1f86792f42aed4aa404a9ae2413b
Dan Reynolds <dreynolds2712@gmail.com>
2021-03-12 16:44:21 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import setuptools
-
+name = "steelforge_site_utils"
+print(name)
 setuptools.setup(
-    name="steelforge_site_utils",
+    name=name,
     version="0.0.1",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
