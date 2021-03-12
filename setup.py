3f799f50fbe5e9caf13d1dc1896fb22ccb754eee
Dan Reynolds <dreynolds2712@gmail.com>
2021-03-12 17:56:31 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ print(name)
 setuptools.setup(
     name=name,
     version="0.0.2",
-    package_dir={"": "steelforge_site_utils"},
-    packages=setuptools.find_packages(where="steelforge_site_utils"),
+    packages=["steelforge_site_utils"],
     python_requires=">=3.6",
 
 )
